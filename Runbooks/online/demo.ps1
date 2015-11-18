a511f033eb1bbe1dd841ebc2f2c3c5d6f7f28ba3
Michael Rueefli <drmiru@hotmail.com>
2015-11-18 03:20:07 -0800

    Updated demo.ps1 in Azure Automation.

--- Runbooks/online/demo.ps1
+++ Runbooks/online/demo.ps1
@@ -1,5 +1,14 @@
 workflow demo
 {
-	write-output "hello world"
-	Write-Output "TechNet Rocks!"
+	param(
+		[Parameter(mandatory=$true)]
+		[string]$Message
+	)
+	
+	$localdir = "C:\DSC\AzureDemo"
+	If (!(test-path -path $localdir))
+	{
+		Write-Verbose "Local Directory does not exist"
+	}
+	$Message | Out-File "$localdir\message.txt" -force
 }
